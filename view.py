d27289c719fa025fcbfa686ac86110ff3e8ca888
Michał Kalke <michkal943@student.polsl.pl>
2022-10-25 19:09:15 +0200

    2 gui bad images

--- view.py
+++ view.py
@@ -1,64 +1,25 @@
-import pygame as pg
-import pygame.mouse
-from pygame import QUIT
+import PySimpleGUI as sg
 
-pg.init()
+def main():
+    end_btn = [sg.Button("X", size=(10, 5))]
+    inny = [sg.Button("go", size=(10, 5))]
+    backgroud = [sg.Image('images/carbon.jpeg', size=(800, 480))]
+    label = [sg.Text('Hello', background_color=("red"))]
+    layout = [label, end_btn, inny, backgroud]
 
-width, height = 800, 480
-window = pg.display.set_mode((width, height), pg.NOFRAME)
+    #create the window
+    window = sg.Window("Demo", layout, size=(800, 480))
 
-background = pg.image.load('images/carbon.jpg')
-background = pg.transform.scale(background, (width, height))
-window.blit(background, (0, 0))
-end = pg.image.load('images/end.png').convert_alpha()
-arr = pg.PixelArray(end)
-arr.replace((0,0,0), (184, 28, 17))
-del arr
+    #event loop
+    while True:
+        event, values = window.read()
+        # if close window or press ok button
+        if  event == sg.WIN_CLOSED or event == 'X':
+            break
+        if event == 'go':
+            print("elo")
 
+    window.close()
 
-# button
-class Button():
-    def __init__(self, image, x, y, scale):
-        width_img = image.get_width()
-        height_img = image.get_height()
-        self.image = pg.transform.scale(image, (int(width_img * scale), int(height_img * scale)))
-        self.rect = self.image.get_rect()
-        self.rect.center = (x, y)
-        self.clicked = False
-
-    def draw(self):
-        action = False
-        # get mouse position
-        pos = pg.mouse.get_pos()
-
-        # check mouseover and clicked conditions
-        if self.rect.collidepoint(pos):
-            if pg.mouse.get_pressed()[0] == 1 and self.clicked == False:
-                self.clicked = True
-                action = True
-
-        if pygame.mouse.get_pressed()[0] == 0:
-            self.clicked = False
-
-        # draw button on screen
-        window.blit(self.image, (self.rect.x, self.rect.y))
-
-        return action
-
-# button instances
-end_btn = Button(end, 750, 50,0.8)
-
-# game loop
-running = True
-while running:
-
-    if end_btn.draw():
-        running = False
-
-    # event handler
-    for event in pg.event.get():
-        # quit game
-        if event.type == QUIT:
-            running = False
-    pg.display.update()
-pg.quit()
+if __name__ == '__main__':
+    main()
